fcce39f50595702f445cee275b27ea2407277b6d
jureck <prawiepro1337@gmail.com>
2020-06-04 12:26:37 +0200

    Updates

---
+++ precache-manifest.6fd89f48ff75861af4a487f3a6c10d0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a570cf346a71986de6a633f6ee525dc",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "a81528749422d1baf38b",
+    "url": "/movies-app/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ec1d59a002401ee50e80",
+    "url": "/movies-app/static/css/main.1f1b4a52.chunk.css"
+  },
+  {
+    "revision": "a81528749422d1baf38b",
+    "url": "/movies-app/static/js/2.a4716464.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/movies-app/static/js/2.a4716464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1d59a002401ee50e80",
+    "url": "/movies-app/static/js/main.bc862f6e.chunk.js"
+  },
+  {
+    "revision": "2b76498351ee2df7cef7",
+    "url": "/movies-app/static/js/runtime-main.48b6978c.js"
+  },
+  {
+    "revision": "e9716c464ae970e648f416c3548566a6",
+    "url": "/movies-app/static/media/check.e9716c46.svg"
+  },
+  {
+    "revision": "7012414554657947feeccd70d2f594e9",
+    "url": "/movies-app/static/media/home.70124145.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/movies-app/static/media/search.5437e521.svg"
+  },
+  {
+    "revision": "4540d5f020eb959a1d074a947f44786f",
+    "url": "/movies-app/static/media/watch.4540d5f0.svg"
+  }
+]);
